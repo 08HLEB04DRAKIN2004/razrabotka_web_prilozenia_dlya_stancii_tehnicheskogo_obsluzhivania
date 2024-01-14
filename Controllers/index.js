63dca994c68ea110164862d137943461c884d2a2
Ilya Lobanovskiy <138199428+08HLEB04DRAKIN2004@users.noreply.github.com>
2024-01-14 13:47:05 +0300

    feat: add pathes

--- Controllers/index.js
+++ Controllers/index.js
@@ -1,5 +1,5 @@
 export * as userController from './userController.js';
-export * as servicesControllers from './servicesControllers.js';
+export * as servicesController from './servicesController.js';
 export * as ReviewController from './ReviewController.js';
 export * as partsController from './partsController.js';
 export * as orderController from './orderController.js';
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import mongoose from 'mongoose'; 
+import mongoose from 'mongoose';
 import multer from 'multer';
 import cors from 'cors';
 
@@ -11,7 +11,7 @@ import {
 
 import {
     userController,
-    servicesControllers,
+    servicesController,
     ReviewController,
     partsController,
     orderController,
@@ -33,27 +33,27 @@ import {
     updateEmployeeValidate,
 } from './validations.js';
 
-mongoose 
+mongoose
     .connect('mongodb+srv://admin:Hesus2016@cluster0.vgtv5yo.mongodb.net/CarService')
     .then(() => console.log('DB OK'))
-    .catch((err) => console.log('DB ERROR', err)); 
+    .catch((err) => console.log('DB ERROR', err));
 
 const app = express();
 
-const storage = multer.diskStorage({ 
-    destination: (_, __, cb) => { 
-        cb(null, 'uploads') 
-    }, 
-    filename: (_, file, cb) => { 
-        cb(null, file.originalname) 
-    }, 
+const storage = multer.diskStorage({
+    destination: (_, __, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (_, file, cb) => {
+        cb(null, file.originalname)
+    },
 });
 
 const upload = multer({ storage });
 
-app.use(express.json()); 
+app.use(express.json());
 app.use(cors());
-app.use('/uploads', express.static('uploads')); 
+app.use('/uploads', express.static('uploads'));
 
 // media upload pathes
 app.post('/upload', adminOnlyAuth, upload.single('image'), (req, res) => {
@@ -66,6 +66,102 @@ app.post('/upload', adminOnlyAuth, upload.single('image'), (req, res) => {
 app.post('/auth/login', loginValidation, handleValidationErrors, userController.login);
 app.post('/auth/register', registerValidation, handleValidationErrors, userController.register);
 app.get('/auth/me', allRolesAuth, userController.getMe);
+
+//services
+app.post('/services/create', adminOnlyAuth, createServiceValidation, handleValidationErrors, servicesController.create);
+app.delete('/services/:id/delte', adminOnlyAuth, servicesController.remove);
+app.patch('/services/:id/update', adminOnlyAuth, updateServiceValidation, handleValidationErrors, servicesController.update);
+app.get('/services/:id', servicesController.getOne);
+app.get('/services', servicesController.getAll);
+
+//reviews
+app.post('/reviews/:orderId/create',
+    allRolesAuth,
+    createReviewValidation,
+    handleValidationErrors,
+    ReviewController.create
+);
+app.delete('/reviews/:reviewId/delete', allRolesAuth, ReviewController.remove);
+app.patch('/reviews/:reviewId/update', adminOnlyAuth, updateReviewValidation, handleValidationErrors, ReviewController.update)
+app.get('/reviews', ReviewController.getAllReviews)
+
+//parts
+app.post('/parts/create',
+    adminOnlyAuth,
+    createPartValidation,
+    handleValidationErrors,
+    partsController.create
+);
+app.delete('/parts/:id/delete', 
+    adminOnlyAuth, 
+    partsController.remove
+);
+app.patch('/parts/:id/update', 
+    adminOnlyAuth, 
+    updatePartValidation, 
+    handleValidationErrors, 
+    partsController.update
+);
+app.get('/parts/:id', 
+    allRolesAuth, 
+    partsController.getOne
+);
+app.get('/parts', 
+    allRolesAuth,
+    partsController.getAll
+);
+
+//orders
+app.post('/orders/create', 
+    allRolesAuth, 
+    createOrderValidation, 
+    handleValidationErrors, 
+    orderController.create
+);
+app.delete('/orders/:id/delete', 
+    adminOnlyAuth, 
+    orderController.remove
+);
+app.patch('/orders/:id/update', 
+    adminOnlyAuth, 
+    updateOrderValidation, 
+    handleValidationErrors, 
+    orderController.update
+);
+app.get('/orders/:id', 
+    allRolesAuth, 
+    orderController.getOne
+);
+app.get('/orders', 
+    adminOnlyAuth, 
+    orderController.getAll
+);
+
+//employees
+app.post('/employees/create', 
+    adminOnlyAuth, 
+    createEmployeeValidate, 
+    handleValidationErrors, 
+    employeeController.create
+);
+app.delete('/employees/:id/delete', 
+    adminOnlyAuth, 
+    employeeController.remove
+);
+app.patch('/employees/:id/update', 
+    adminOnlyAuth, 
+    updateEmployeeValidate, 
+    handleValidationErrors, 
+    employeeController.update
+);
+app.get('/employees/:id', 
+    allRolesAuth, 
+    employeeController.getOne
+);
+app.get('/employees', 
+    adminOnlyAuth, 
+    employeeController.getAll
+);
 
 app.listen(4444, (err) => {
     if (err) {
